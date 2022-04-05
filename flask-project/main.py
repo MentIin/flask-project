e70ff5156e3f7d56b440dba49c369175341857b1
MentIin <miheevdeman776@gmail.com>
2022-04-05 21:05:41 +0200

    add user avatar

--- flask-project/main.py
+++ flask-project/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
@@ -7,9 +7,13 @@ from data.users import User
 from data.posts import Post
 from data.likes import Likes
 from flask_login import *
+import os
+import datetime
+from pathlib import Path
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aba43urg4d78g2983g_key'
+
 app.debug = True
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -65,15 +69,42 @@ def index():
             }
         ]
     }
-    return render_template("index.html", posts=posts_list, page=page)
+    return render_template("index.html", posts=posts_list, page=page, title="Lenta")
 
 
-@app.route("/profile", methods=['GET'])
+@app.route("/profile", methods=['GET', "POST"])
 def profile():
+    if request.method == 'POST':
+        file = request.files["avatar"]
+        current_time = datetime.datetime.now()
+        path = Path("static", "avatars")
+        folder = str(current_time.date())
+        filename = str(current_user.id) \
+                   + str(current_time.time()).replace(":", "-").replace(".", "-") \
+                   + ".png"
+        # Если файл не выбран, то браузер может
+        # отправить пустой файл без имени.
+        if file.filename == '':
+            flash('Нет выбранного файла')
+            return redirect(request.url)
+
+        # сохраняем файл
+        path = Path("static", "avatars", folder)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        file.save(os.path.join(path, filename))
+
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.id == current_user.id).first()
+        user.avatar_im_path = os.path.join(folder, filename)
+        db_sess.commit()
+        return redirect(request.url)
     if current_user.is_authenticated:
         last_page = request.args.get("last_page")
-
-        return render_template("profile.html", last_page=last_page)
+        avatar_path = Path("static", "avatars", current_user.avatar_im_path)
+        print(avatar_path)
+        return render_template("profile.html", avatar=avatar_path,
+                               last_page=last_page, title=current_user.login)
     else:
         return redirect("/login")
 
@@ -90,7 +121,7 @@ def login():
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, title="Вход")
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -117,7 +148,7 @@ def registration():
             return render_template('registration.html',
                                    message="Пользователь с таким логином уже существует",
                                    form=form)
-    return render_template('registration.html', form=form)
+    return render_template('registration.html', form=form, title="Регистрация")
 
 
 @login_manager.user_loader
